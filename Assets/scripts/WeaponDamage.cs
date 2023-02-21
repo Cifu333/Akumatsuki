901158cdf7c6fc209da4bff5b3e4958bca89cfd7
PC-imatge-2022-2023 <licenses@enti.cat>
2023-02-21 12:14:49 +0100

    escena demo creada

--- Assets/scripts/WeaponDamage.cs
+++ Assets/scripts/WeaponDamage.cs
@@ -9,7 +9,6 @@ public class WeaponDamage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        hazardus = true;
     }
 
     // Update is called once per frame

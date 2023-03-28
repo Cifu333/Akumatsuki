3989b5519c7f34966f2ce41f10f06149c38a6a95
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-28 13:06:59 +0200

    camshake escalable hecho

--- Assets/scripts/Camera/camShake.cs
+++ Assets/scripts/Camera/camShake.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camShake : MonoBehaviour {
-    public float duration = 1.0f;
-    public bool shakebb;
+    public float duration = 0.5f;
+    public bool pressToShake1;
+    public bool pressToShake2;
+    public bool pressToShake3;
 
-    public AnimationCurve curve;
+    public AnimationCurve curve1;
+    public AnimationCurve curve2;
+    public AnimationCurve curve3;
     private void Update() {
-        if (shakebb) {
-            StartCoroutine(Shaking());
+        if (pressToShake1) {
+            Shake1();
+            pressToShake1 = false;
         }
-        shakebb = false;
+        if (pressToShake2) {
+            Shake2();
+            pressToShake2 = false;
+        }
+        if (pressToShake3) {
+            Shake3();
+            pressToShake3 = false;
+        }
     }
 
-    public IEnumerator Shaking() {
+    public IEnumerator Shaking(AnimationCurve curve) {
         Vector3 startPosition = transform.position;
         float elapsedTime = 0.0f;
 
@@ -28,8 +40,19 @@ public class camShake : MonoBehaviour {
         transform.position = startPosition;
     }
 
-    public void Shake() {
-        StartCoroutine(Shaking());
+    public void Shake1() {
+        StartCoroutine(Shaking(curve1));
+
+        return;
+    }
+
+    public void Shake2() {
+        StartCoroutine(Shaking(curve2));
+
+        return;
+    }
+    public void Shake3() {
+        StartCoroutine(Shaking(curve3));
 
         return;
     }

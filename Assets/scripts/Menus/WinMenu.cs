35a21cd2f5cc4e33194c765b372505546b62aaaa
PC-imatge-2022-2023 <licenses@enti.cat>
2023-05-10 19:59:01 +0200

    no message

--- Assets/scripts/Menus/WinMenu.cs
+++ Assets/scripts/Menus/WinMenu.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class WinMenu : MonoBehaviour
 {
     public static WinMenu instance;
-    public GameObject pauseMenu;
+    public GameObject BadMenu;
+    public GameObject GoodMenu;
+    public GameObject NeutralMenu;
+    GameObject player;
+    PlayerAbilitys pa;
     public bool isPaused
     {
         get;
@@ -16,26 +20,48 @@ public class WinMenu : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        pa = player.GetComponent<PlayerAbilitys>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu.SetActive(false);
+        BadMenu.SetActive(false);
+        GoodMenu.SetActive(false);
+        NeutralMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.GetComponent<PlayerStatus>().win == true && pa.demon[0] == true && pa.demon[1] == true)
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+            BadMenu.SetActive(true);
 
+        }
+        if (player.GetComponent<PlayerStatus>().win == true && pa.demon[0] == true && pa.human[0] == true)
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+            NeutralMenu.SetActive(true);
+
+        }
+        if (player.GetComponent<PlayerStatus>().win == true && pa.human[0] == true && pa.human[1] == true)
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+            GoodMenu.SetActive(true);
+
+        }
     }
 
     public void Continue()
     {
-        SceneManager.LoadScene("demo");
         isPaused = false;
         Time.timeScale = 1;
-        pauseMenu.SetActive(false);
-        Debug.Log("Loading");
+        SceneManager.LoadScene(0);
     }
 }
